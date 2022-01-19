a773c4e96f518ae0d3e670e7cbab55c21c18144c
David Anthoff <anthoff@berkeley.edu>
2022-01-18 16:14:09 -0800

    Format everything

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,36 @@
 extern crate yaserde_derive;
 pub mod packagelayout;
 
-use std::{path::PathBuf, env::{current_dir, set_current_dir}};
+use std::{
+    env::{current_dir, set_current_dir},
+    path::PathBuf,
+};
 
+use anyhow::{anyhow, Result};
 use cargo_metadata::camino::Utf8PathBuf;
 use clap::Parser;
 use log::info;
-use anyhow::{anyhow,Result, bail};
-use windows::{Win32::{System::Com::{CoInitialize, StructuredStorage::{STGM_CREATE, STGM_WRITE, STGM_SHARE_EXCLUSIVE, STGM_READ}, CreateUri, Uri_CREATE_CANONICALIZE, CoCreateInstance, CLSCTX_INPROC_SERVER, IStream}, UI::Shell::{SHCreateStreamOnFileEx, SHCreateMemStream}, Storage::Packaging::Appx::{APPX_PACKAGE_SETTINGS, IAppxFactory, AppxFactory, IAppxPackageWriter, APPX_COMPRESSION_OPTION_MAXIMUM, IAppxBundleWriter, IAppxBundleFactory, AppxBundleFactory, IAppxBundleWriter4}, Foundation::BOOL}, core::Interface};
+use windows::{
+    core::Interface,
+    Win32::{
+        Foundation::BOOL,
+        Storage::Packaging::Appx::{
+            AppxBundleFactory, AppxFactory, IAppxBundleFactory, IAppxBundleWriter,
+            IAppxBundleWriter4, IAppxFactory, IAppxPackageWriter, APPX_COMPRESSION_OPTION_MAXIMUM,
+            APPX_PACKAGE_SETTINGS,
+        },
+        System::Com::{
+            CoCreateInstance, CoInitialize, CreateUri, IStream,
+            StructuredStorage::{STGM_CREATE, STGM_READ, STGM_SHARE_EXCLUSIVE, STGM_WRITE},
+            Uri_CREATE_CANONICALIZE, CLSCTX_INPROC_SERVER, STREAM_SEEK_SET,
+        },
+        UI::Shell::{SHCreateMemStream, SHCreateStreamOnFileEx},
+    },
+};
 use yaserde::de::from_str;
 
 #[derive(Parser)]
-#[clap(name="cargo-msix", version)]
+#[clap(name = "cargo-msix", version)]
 struct Cli {
     #[clap(flatten)]
     manifest: clap_cargo::Manifest,
@@ -49,7 +68,7 @@ fn main() -> Result<()> {
     let metadata = metadata_cmd.exec().unwrap();
 
     let profile = if args.release { "release" } else { "debug" };
-    
+
     run_command_default(&args, &metadata, &profile.to_string())?;
 
     Ok(())
@@ -69,24 +88,20 @@ fn create_appx_package_writer(stream: &IStream) -> Result<IAppxPackageWriter> {
         CreateUri(
             "http://www.w3.org/2001/04/xmlenc#sha256",
             Uri_CREATE_CANONICALIZE,
-            0)
-        }.unwrap();
+            0,
+        )
+    }
+    .unwrap();
 
     let writer_settings = APPX_PACKAGE_SETTINGS {
         forceZip32: BOOL::from(false),
         hashMethod: Some(hash_method),
     };
 
-    let appx_factory: IAppxFactory = unsafe {
-        CoCreateInstance(
-            &AppxFactory,
-            None,
-            CLSCTX_INPROC_SERVER).unwrap()
-    };
+    let appx_factory: IAppxFactory =
+        unsafe { CoCreateInstance(&AppxFactory, None, CLSCTX_INPROC_SERVER).unwrap() };
 
-    let writer = unsafe {
-        appx_factory.CreatePackageWriter(stream, &writer_settings)
-    }.unwrap();
+    let writer = unsafe { appx_factory.CreatePackageWriter(stream, &writer_settings) }.unwrap();
 
     Ok(writer)
 }
@@ -98,27 +113,28 @@ fn create_appx_bundle_writer(filename: &PathBuf) -> Result<IAppxBundleWriter> {
             STGM_CREATE | STGM_WRITE | STGM_SHARE_EXCLUSIVE,
             0, // default file attribute
             true,
-            None)
-    }.unwrap();
-
-    let appx_bundle_factory: IAppxBundleFactory = unsafe {
-        CoCreateInstance(
-            &AppxBundleFactory,
             None,
-            CLSCTX_INPROC_SERVER).unwrap()
-    };
+        )
+    }
+    .unwrap();
 
-    let writer = unsafe {
-        appx_bundle_factory.CreateBundleWriter(stream, 0)
-    }.unwrap();
+    let appx_bundle_factory: IAppxBundleFactory =
+        unsafe { CoCreateInstance(&AppxBundleFactory, None, CLSCTX_INPROC_SERVER).unwrap() };
+
+    let writer = unsafe { appx_bundle_factory.CreateBundleWriter(stream, 0) }.unwrap();
 
     Ok(writer)
 }
 
-fn create_manifest(cli_args: &Cli, appmanifest_path: &PathBuf, version: &str, processor_architecture: &str) -> Result<IStream> {
+fn create_manifest(
+    cli_args: &Cli,
+    appmanifest_path: &PathBuf,
+    version: &str,
+    processor_architecture: &str,
+) -> Result<IStream> {
     let template = mustache::compile_path(&appmanifest_path).unwrap();
     let data = mustache::MapBuilder::new()
-        .insert_str("Version", version.to_string() + ".0")
+        .insert_str("Version", version.to_string())
         .insert_str("ProcessorArchitecture", processor_architecture)
         .build();
     let manifestcontent = template.render_data_to_string(&data).unwrap();
@@ -127,31 +143,41 @@ fn create_manifest(cli_args: &Cli, appmanifest_path: &PathBuf, version: &str, pr
 
     if cli_args.unsigned {
         let identity_element = parsedcontent
-            .get_child_mut("Identity", "http://schemas.microsoft.com/appx/manifest/foundation/windows10").unwrap();    
+            .get_child_mut(
+                "Identity",
+                "http://schemas.microsoft.com/appx/manifest/foundation/windows10",
+            )
+            .unwrap();
         let old_publisher = identity_element.attr("Publisher").unwrap();
-        let new_publisher = format!("{old_publisher}, OID.2.25.311729368913984317654407730594956997722=1");
+        let new_publisher =
+            format!("{old_publisher}, OID.2.25.311729368913984317654407730594956997722=1");
         identity_element.set_attr("Publisher", new_publisher);
     }
 
     let manifestcontent = String::from(&parsedcontent);
 
-    let manifest_stream = unsafe {
-        SHCreateMemStream(manifestcontent.as_ptr(), manifestcontent.len() as u32)
-    }.unwrap();
+    let manifest_stream =
+        unsafe { SHCreateMemStream(manifestcontent.as_ptr(), manifestcontent.len() as u32) }
+            .unwrap();
 
     Ok(manifest_stream)
 }
 
-fn run_command_default(cli_args: &Cli, metadata: &cargo_metadata::Metadata, profile: &String) -> Result<()> {
+fn run_command_default(
+    cli_args: &Cli,
+    metadata: &cargo_metadata::Metadata,
+    profile: &String,
+) -> Result<()> {
     let root_package = metadata.root_package().unwrap();
 
     let output_root_path = metadata.target_directory.join("msix");
     std::fs::create_dir_all(&output_root_path).unwrap();
-    
 
-    
-
-    for (_bundle_name, bundle_packagelayout_path_as_string) in metadata.root_package().unwrap().metadata["msix"].as_object().unwrap() {
+    for (_bundle_name, bundle_packagelayout_path_as_string) in
+        metadata.root_package().unwrap().metadata["msix"]
+            .as_object()
+            .unwrap()
+    {
         let mut bundle_packagelayout_path = Utf8PathBuf::from(&metadata.workspace_root);
         bundle_packagelayout_path.push(bundle_packagelayout_path_as_string.as_str().unwrap());
         let bundle_packagelayout_path = bundle_packagelayout_path.canonicalize().unwrap();
@@ -162,57 +188,102 @@ fn run_command_default(cli_args: &Cli, metadata: &cargo_metadata::Metadata, prof
 
         let packagelayout_template = mustache::compile_path(&bundle_packagelayout_path).unwrap();
         let data = mustache::MapBuilder::new()
-            .insert_str("version", root_package.version.to_string() + ".0")
-         .build();
+            .insert_str("Version", root_package.version.to_string())
+            .build();
         let packagelayout_content = packagelayout_template.render_data_to_string(&data).unwrap();
 
-        let packagelayout_parsed: packagelayout::PackagingLayout = from_str(&packagelayout_content).unwrap();
+        let packagelayout_parsed: packagelayout::PackagingLayout =
+            from_str(&packagelayout_content).unwrap();
 
-        let output_name = format!("{}.msixbundle", packagelayout_parsed.child.id);
-        let output_path = output_root_path.join(&output_name);
+        for package_family in packagelayout_parsed.package_families {
+            let output_name = package_family.filename;
+            let output_path = output_root_path.join(&output_name);
 
-        if output_path.exists() {
-            std::fs::remove_file(&output_path).unwrap();
-        }
+            if output_path.exists() {
+                std::fs::remove_file(&output_path).unwrap();
+            }
 
-        let mut manifest_path = Utf8PathBuf::from(bundle_packagelayout_path.parent().unwrap().to_string_lossy().to_string());
-        manifest_path.push(packagelayout_parsed.child.manifest_path);
-        let manifest_path = manifest_path.canonicalize().unwrap();
+            let mut manifest_path = Utf8PathBuf::from(
+                bundle_packagelayout_path
+                    .parent()
+                    .unwrap()
+                    .to_string_lossy()
+                    .to_string(),
+            );
+            manifest_path.push(package_family.manifest_path);
+            let manifest_path = manifest_path.canonicalize().unwrap();
 
-        let appx_bundle_writer = create_appx_bundle_writer(&output_path.as_std_path().to_path_buf()).unwrap();
+            let appx_bundle_writer =
+                create_appx_bundle_writer(&output_path.as_std_path().to_path_buf()).unwrap();
 
-        for package in packagelayout_parsed.child.children {
-            let manifest_stream = create_manifest(cli_args, &manifest_path, &format!("{}.{}.{}", root_package.version.major, root_package.version.minor, root_package.version.patch), &package.processor_architecture).unwrap();
+            for package in package_family.packages {
+                let manifest_stream = create_manifest(
+                    cli_args,
+                    &manifest_path,
+                    &format!(
+                        "{}.{}.{}",
+                        root_package.version.major,
+                        root_package.version.minor,
+                        root_package.version.patch
+                    ),
+                    &package.processor_architecture,
+                )
+                .unwrap();
 
-            let package_stream = unsafe {
-                SHCreateMemStream(std::ptr::null_mut(), 0)
-            }.unwrap();
+                let package_stream = if !package_family.flat_bundle {
+                    unsafe { SHCreateMemStream(std::ptr::null_mut(), 0) }.unwrap()
+                } else {
+                    unsafe {
+                        SHCreateStreamOnFileEx(
+                            output_root_path.join(&package.filename).to_string(),
+                            STGM_CREATE | STGM_READ | STGM_WRITE | STGM_SHARE_EXCLUSIVE,
+                            0,
+                            false,
+                            None,
+                        )
+                    }
+                    .unwrap()
+                };
 
-            let appx_package_writer = create_appx_package_writer(&package_stream).unwrap();
+                let appx_package_writer = create_appx_package_writer(&package_stream).unwrap();
 
-            for file in package.files.children {
-                if file.source_path.is_some() {
-                        let filepath = bundle_packagelayout_path.parent().unwrap().join(file.source_path.unwrap());
+                for file in package.files.files {
+                    let filepath = bundle_packagelayout_path
+                        .parent()
+                        .unwrap()
+                        .join(file.source_path);
 
-                        let filestream = unsafe {
-                            SHCreateStreamOnFileEx(
-                                filepath.to_string_lossy().to_string(),
-                                STGM_READ | STGM_SHARE_EXCLUSIVE,
-                                0,
-                                false,
-                                None
-                            )
-                        }.unwrap();
-                                
-                        unsafe {
-                            appx_package_writer.AddPayloadFile(file.destination_path.unwrap(), "application/octet-stream", APPX_COMPRESSION_OPTION_MAXIMUM, filestream)
-                        }.unwrap();
+                    let filestream = unsafe {
+                        SHCreateStreamOnFileEx(
+                            filepath.to_string_lossy().to_string(),
+                            STGM_READ | STGM_SHARE_EXCLUSIVE,
+                            0,
+                            false,
+                            None,
+                        )
+                    }
+                    .unwrap();
+
+                    unsafe {
+                        appx_package_writer.AddPayloadFile(
+                            file.destination_path,
+                            "application/octet-stream",
+                            APPX_COMPRESSION_OPTION_MAXIMUM,
+                            filestream,
+                        )
+                    }
+                    .unwrap();
                 }
-                else if file.source_target.is_some() {
-                    let source_target = file.source_target.unwrap();
-                    let source_platform = file.source_platform.unwrap();
 
-                    let filepath = metadata.target_directory.join(source_platform).join(profile).join(format!("{source_target}.exe"));
+                for buildoutput in package.files.buildoutputs {
+                    let source_target = buildoutput.source_target;
+                    let source_platform = buildoutput.source_platform;
+
+                    let filepath = metadata
+                        .target_directory
+                        .join(source_platform)
+                        .join(profile)
+                        .join(format!("{source_target}.exe"));
 
                     let filestream = unsafe {
                         SHCreateStreamOnFileEx(
@@ -220,62 +291,104 @@ fn run_command_default(cli_args: &Cli, metadata: &cargo_metadata::Metadata, prof
                             STGM_READ | STGM_SHARE_EXCLUSIVE,
                             0,
                             false,
-                            None
+                            None,
                         )
-                    }.unwrap();
-                            
+                    }
+                    .unwrap();
+
                     unsafe {
-                        appx_package_writer.AddPayloadFile(file.destination_path.unwrap(), "application/octet-stream", APPX_COMPRESSION_OPTION_MAXIMUM, filestream)
-                    }.unwrap();
+                        appx_package_writer.AddPayloadFile(
+                            buildoutput.destination_path,
+                            "application/octet-stream",
+                            APPX_COMPRESSION_OPTION_MAXIMUM,
+                            filestream,
+                        )
+                    }
+                    .unwrap();
                 }
-                else if file.source_root.is_some() {
+
+                for filepattern in package.files.filepatterns {
                     let old_working_dir = current_dir().unwrap();
                     let new_working_dir = bundle_packagelayout_path
                         .parent()
                         .unwrap()
-                        .join(file.source_root.unwrap());
+                        .join(filepattern.source_root);
                     set_current_dir(&new_working_dir).unwrap();
 
-                    let destination_root = file.destination_root.unwrap();
+                    let destination_root = filepattern.destination_root;
 
-                    for file2 in glob::glob(&file.source_pattern.unwrap()).unwrap() {
+                    for file2 in glob::glob(&filepattern.source_pattern).unwrap() {
                         let file2 = file2.unwrap();
 
                         let source_filename = new_working_dir.join(&file2);
 
-                        if !source_filename.is_dir() {
+                        if source_filename.is_file() {
                             let filestream = unsafe {
                                 SHCreateStreamOnFileEx(
                                     source_filename.to_string_lossy().to_string(),
                                     STGM_READ | STGM_SHARE_EXCLUSIVE,
                                     0,
                                     false,
-                                    None
+                                    None,
                                 )
-                            }.unwrap();
+                            }
+                            .unwrap();
 
-                            let destination_filename = PathBuf::from(&destination_root).join(&file2);
+                            let destination_filename =
+                                PathBuf::from(&destination_root).join(&file2);
 
                             unsafe {
-                                appx_package_writer.AddPayloadFile(destination_filename.to_string_lossy().to_string(), "application/octet-stream", APPX_COMPRESSION_OPTION_MAXIMUM, filestream)
-                            }.unwrap();
-                        } 
+                                appx_package_writer.AddPayloadFile(
+                                    destination_filename.to_string_lossy().to_string(),
+                                    "application/octet-stream",
+                                    APPX_COMPRESSION_OPTION_MAXIMUM,
+                                    filestream,
+                                )
+                            }
+                            .unwrap();
+                        } else if !source_filename.is_dir() {
+                            eprintln!("Warning: Skipping file {:?}.", source_filename);
+                        }
                     }
 
                     set_current_dir(old_working_dir).unwrap();
                 }
-                else {
-                    bail!("Wrong file format.")
+
+                unsafe { appx_package_writer.Close(&manifest_stream) }.unwrap();
+
+                if package_family.flat_bundle {
+                    unsafe { package_stream.Seek(0, STREAM_SEEK_SET) }.unwrap();
+                    let package_stream = unsafe {
+                        SHCreateStreamOnFileEx(
+                            output_root_path.join(&package.filename).to_string(),
+                            STGM_READ | STGM_SHARE_EXCLUSIVE,
+                            0,
+                            false,
+                            None,
+                        )
+                    }
+                    .unwrap();
+
+                    unsafe {
+                        appx_bundle_writer
+                            .cast::<IAppxBundleWriter4>()
+                            .unwrap()
+                            .AddPackageReference(package.filename, &package_stream, false)
+                    }
+                    .unwrap();
+                } else {
+                    unsafe {
+                        appx_bundle_writer
+                            .cast::<IAppxBundleWriter4>()
+                            .unwrap()
+                            .AddPayloadPackage(package.filename, &package_stream, false)
+                    }
+                    .unwrap();
                 }
-                
             }
 
-            unsafe { appx_package_writer.Close(&manifest_stream) }.unwrap();
-
-            unsafe{ appx_bundle_writer.cast::<IAppxBundleWriter4>().unwrap().AddPayloadPackage(format!("{}.msix", package.id), &package_stream, false) }.unwrap();
+            unsafe { appx_bundle_writer.Close() }.unwrap();
         }
-
-        unsafe { appx_bundle_writer.Close() }.unwrap();
     }
 
     Ok(())
--- src/packagelayout.rs
+++ src/packagelayout.rs
@@ -1,27 +1,25 @@
 #[derive(Default, PartialEq, Debug, YaDeserialize)]
 pub struct PackagingLayout {
     #[yaserde(child, rename = "PackageFamily")]
-    pub child: PackageFamily
+    pub package_families: Vec<PackageFamily>,
 }
 
 #[derive(Default, PartialEq, Debug, YaDeserialize)]
 pub struct PackageFamily {
-    #[yaserde(attribute, rename = "ID")]
-    pub id: String,
+    #[yaserde(attribute, rename = "Filename")]
+    pub filename: String,
     #[yaserde(attribute, rename = "FlatBundle")]
     pub flat_bundle: bool,
     #[yaserde(attribute, rename = "ManifestPath")]
     pub manifest_path: String,
-    #[yaserde(attribute, rename = "ResourceManager")]
-    pub resource_manager: bool,
     #[yaserde(rename = "Package")]
-    pub children: Vec<Package>,
+    pub packages: Vec<Package>,
 }
 
 #[derive(Default, PartialEq, Debug, YaDeserialize)]
 pub struct Package {
-    #[yaserde(attribute, rename = "ID")]
-    pub id: String,
+    #[yaserde(attribute, rename = "Filename")]
+    pub filename: String,
     #[yaserde(attribute, rename = "ProcessorArchitecture")]
     pub processor_architecture: String,
     #[yaserde(rename = "Files")]
@@ -31,23 +29,37 @@ pub struct Package {
 #[derive(Default, PartialEq, Debug, YaDeserialize)]
 pub struct Files {
     #[yaserde(rename = "File")]
-    pub children: Vec<File>,
+    pub files: Vec<File>,
+    #[yaserde(rename = "FilePattern")]
+    pub filepatterns: Vec<FilePattern>,
+    #[yaserde(rename = "BuildOutput")]
+    pub buildoutputs: Vec<BuildOutput>,
+}
+
+#[derive(Default, PartialEq, Debug, YaDeserialize)]
+pub struct FilePattern {
+    #[yaserde(attribute, rename = "DestinationRoot")]
+    pub destination_root: String,
+    #[yaserde(attribute, rename = "SourceRoot")]
+    pub source_root: String,
+    #[yaserde(attribute, rename = "SourcePattern")]
+    pub source_pattern: String,
+}
+
+#[derive(Default, PartialEq, Debug, YaDeserialize)]
+pub struct BuildOutput {
+    #[yaserde(attribute, rename = "DestinationPath")]
+    pub destination_path: String,
+    #[yaserde(attribute, rename = "SourceTarget")]
+    pub source_target: String,
+    #[yaserde(attribute, rename = "SourcePlatform")]
+    pub source_platform: String,
 }
 
 #[derive(Default, PartialEq, Debug, YaDeserialize)]
 pub struct File {
     #[yaserde(attribute, rename = "DestinationPath")]
-    pub destination_path: Option<String>,
-    #[yaserde(attribute, rename = "DestinationRoot")]
-    pub destination_root: Option<String>,
+    pub destination_path: String,
     #[yaserde(attribute, rename = "SourcePath")]
-    pub source_path: Option<String>,
-    #[yaserde(attribute, rename = "SourceRoot")]
-    pub source_root: Option<String>,
-    #[yaserde(attribute, rename = "SourcePattern")]
-    pub source_pattern: Option<String>,
-    #[yaserde(attribute, rename = "SourceTarget")]
-    pub source_target: Option<String>,
-    #[yaserde(attribute, rename = "SourcePlatform")]
-    pub source_platform: Option<String>,
+    pub source_path: String,
 }
